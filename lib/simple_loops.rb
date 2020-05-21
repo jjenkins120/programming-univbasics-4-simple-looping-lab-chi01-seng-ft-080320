e112639b9a6c7a294d226f76b4ca6866ab678304
Jeff Jenkins <jjenkins120@gmail.com>
2020-05-21 17:39:28 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,11 @@
 # Write your methods here
 
-def loop_message_five_times
-  while do 
+
+def loop_message_five_times(message)
+  counter = 0 
+  
+  while message(counter) do 
+  puts message  
+  counter += 4
   end
 end 
